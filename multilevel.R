86ed25c82532403ebae067eeb3351bb4973a6908
arturoto <arturoto@buffalo.edu>
2018-07-27 00:50:42 -0400

    R code for multilevel

---
+++ multilevel.R
@@ -0,0 +1,46 @@
+"
+Simple Multilevel Models with nlme 
+
+Intercept Only
+
+"
+library(nlme)
+
+Achieve <- read.table('./Datasets/Achieve.csv', header=TRUE, sep=',')
+
+model3.0 <- lme (fixed= geread~1 , random = ~1| school, data = Achieve)
+
+summary(model3.0)
+
+" Adding independent variable"
+
+model3.1 <- lme(fixed= geread~gevocab , random = ~1| school, data = Achieve)
+
+
+summary(model3.1)
+
+"
+What if school size also had something to do with the predictor variable.
+add at level 2
+"
+
+model3.2 <- lme(fixed = geread~gevocab + senroll, random = ~1|school, data = Achieve)
+
+summary(model3.2)
+
+"
+Allowing for geovocab to be both fixed and random
+"
+
+
+
+
+
+
+
+
+
+
+
+
+
