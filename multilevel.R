21332e4e77d8eb0f87e71b8dc36ac0caa8322fc5
Arturo Torres <atorres@Arturos-MacBook-Pro.local>
2019-02-02 18:26:20 -0500

    ICC

--- multilevel.R
+++ multilevel.R
@@ -1,47 +1,50 @@
-"
-Simple Multilevel Models with nlme 
-
-Intercept Only
-
-"
 library(nlme)
 
-Achieve <- read.table('./Datasets/Achieve.csv', header=TRUE, sep=',')
+"
+Simple Linear Model
 
-model3.0 <- lme (fixed= geread~1 , random = ~1| school, data = Achieve)
+Starting with Null Model. A model that has no independant variables. This model is useful for
+obtaining estimates of the residual and intercept variance when only clustering is considered.
+"
 
+Achieve <- read.table('/Users/atorres/desktop/Multilevel-Modeling/Datasets/Achieve.csv', header=TRUE, sep=',')
+
+model3.0 <- lme(fixed= geread~1, random = ~1|school, data=Achieve)
 summary(model3.0)
+"
+output:
+Linear mixed-effects model fit by REML
+ Data: Achieve 
+    AIC      BIC    logLik
+46274.31 46296.03 -23134.15
 
-" Adding independent variable"
+Random effects:
+Formula: ~1 | school
+(Intercept) Residual
+StdDev:   0.6257119  2.24611
 
-model3.1 <- lme(fixed= geread~gevocab , random = ~1| school, data = Achieve)
+Fixed effects: geread ~ 1 
+              Value  Std.Error    DF t-value p-value
+(Intercept) 4.306753 0.05497501 10160 78.3402       0
 
+Standardized Within-Group Residuals:
+      Min         Q1        Med         Q3        Max 
+-2.3229469 -0.6377948 -0.2137753  0.2849664  3.8811630 
 
-summary(model3.1)
+Number of Observations: 10320
+Number of Groups: 160 
+"
 
 "
-What if school size also had something to do with the predictor variable.
-add at level 2
+From the above model, the AIC and BIC will be the useful when comparing models with more IV's.
+Also:
+sigma^2, the variance among the individuals 
+tao^2, the variance among the clusters.
+
+These values can be used to calculate the ICC, roh1: 
+
+from the StandDev:
+0.6257119^2/(0.6257119^2 + 2.24611^2) = ICC = .072
+
+^^ the correlation of reading test scores among students within the same school is approx .07
 "
-
-model3.2 <- lme(fixed = geread~gevocab + senroll, random = ~1|school, data = Achieve)
-
-summary(model3.2)
-
-"
-Allowing for geovocab to be both fixed and random
-"
-
-
-
-model3.3 <- lme(fixed = geread~gevocab, random = ~gevocab|school, data = Achieve)
-
-summary(model3.3)
-fir
-
-
-
-
-
-
-
